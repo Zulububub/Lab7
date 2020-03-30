09071d89711f4d4cc36b02877baa1b55bd8df409
Zulububub <45508795+Zulububub@users.noreply.github.com>
2020-03-30 10:26:07 +0200

    Add files via upload

---
+++ Lab7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                using (StreamWriter sw = new StreamWriter("Names.txt", true))
+                {
+                    sw.WriteLine(textBox1.Text + " " + textBox2.Text + " " + textBox3.Text);
+                    textBox1.Text = textBox2.Text = textBox3.Text = "";
+                }
+            }
+            else MessageBox.Show("Please, fill all fields!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TableForm form = new TableForm();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Lab7/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class TableForm : Form
+    {
+        public class Names
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string PhoneNumber { get; set; }
+        }
+        public static List<Names> names = new List<Names>();
+        public TableForm()
+        {
+            InitializeComponent();
+            using (StreamReader sr = new StreamReader("Names.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] words = line.Split(new char[] { ' ' });
+                    names.Add(new Names() { Name = words[0], Surname = words[1], PhoneNumber = words[2] });
+                }
+            }
+            dataGridView1.DataSource = names;
+        }
+
+        private void TableForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            names.Clear();
+        }
+    }
+}
